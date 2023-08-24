ae5757bbb82aadc1b245a0c30a65f9263895d3c0
zuzanna-maria <zuzanna.ludzik@gmail.com>
2023-08-25 00:57:14 +0100

    kanban and markdown tests

--- e2e-tests/kanban_anon.spec.js
+++ e2e-tests/kanban_anon.spec.js
@@ -1,6 +1,8 @@
 const { test, expect } = require('@playwright/test');
 const { firefox, chromium, webkit } = require('@playwright/test');
 const { url, titleDate } = require('../browserstack.config.js')
+var fs = require('fs');
+
 
 let page;
 let pageOne;
@@ -242,81 +244,229 @@ test.beforeEach(async ({  }, testInfo) => {
 //   }  
 // });
 
-test(`pad - history (previous author) -- DOESNT WORK`, async ({ }, testInfo) => {
+// test(`pad - history (previous author) -- DOESNT WORK`, async ({ }, testInfo) => {
 
-  try {
+//   try {
 
-    await page.goto(`${url}/login/`)
-    await page.getByPlaceholder('Username').fill('test-user');
-    await page.waitForTimeout(10000)
-    await page.getByPlaceholder('Password', {exact: true}).fill('newpassword');
-    const login = page.locator(".login")
-    await login.waitFor({ timeout: 18000 })
-    await expect(login).toBeVisible({ timeout: 1800 })
-    if (await login.isVisible()) {
-      await login.click()
-    }
-    await expect(page).toHaveURL(`${url}/drive/#`, { timeout: 100000 })
-    await page.goto(`${url}/kanban`)
+//     await page.goto(`${url}/login/`)
+//     await page.getByPlaceholder('Username').fill('test-user');
+//     await page.waitForTimeout(10000)
+//     await page.getByPlaceholder('Password', {exact: true}).fill('newpassword');
+//     const login = page.locator(".login")
+//     await login.waitFor({ timeout: 18000 })
+//     await expect(login).toBeVisible({ timeout: 1800 })
+//     if (await login.isVisible()) {
+//       await login.click()
+//     }
+//     await expect(page).toHaveURL(`${url}/drive/#`, { timeout: 100000 })
+//     await page.goto(`${url}/kanban`)
 
-    await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Create' }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Create' }).click();
 
-    await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().waitFor()
-    await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('test text by test-user');
-    await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
-    await page.waitForTimeout(5000)
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().waitFor()
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('test text by test-user');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await page.waitForTimeout(5000)
 
 
-    await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Share' }).click();
-    await page.frameLocator('#sbox-secure-iframe').getByText('Link', { exact: true }).click();
-    await page.frameLocator('#sbox-secure-iframe').locator('label').filter({ hasText: /^Edit$/ }).locator('span').first().click();
-    await page.frameLocator('#sbox-secure-iframe').getByRole('button', {name: 'Copy link'}).click();
-    const clipboardText = await page.evaluate("navigator.clipboard.readText()");
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Share' }).click();
+//     await page.frameLocator('#sbox-secure-iframe').getByText('Link', { exact: true }).click();
+//     await page.frameLocator('#sbox-secure-iframe').locator('label').filter({ hasText: /^Edit$/ }).locator('span').first().click();
+//     await page.frameLocator('#sbox-secure-iframe').getByRole('button', {name: 'Copy link'}).click();
+//     const clipboardText = await page.evaluate("navigator.clipboard.readText()");
 
-    pageOne = await browser.newPage();
-    await pageOne.goto(`${clipboardText}`)
-    await pageOne.waitForTimeout(5000)
-    await pageOne.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('some test text by anon');
-    await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
-    // await pageOne.waitForTimeout(5000)
-    await pageOne.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await pageOne.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('some more test text by anon!');
-    await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
-    await pageOne.waitForTimeout(5000)
+//     pageOne = await browser.newPage();
+//     await pageOne.goto(`${clipboardText}`)
+//     await pageOne.waitForTimeout(5000)
+//     await pageOne.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('some test text by anon');
+//     await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await pageOne.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await pageOne.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('some more test text by anon!');
+//     await pageOne.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await pageOne.waitForTimeout(9000)
 
-    await pageOne.close()
+//     await pageOne.close()
 
-    await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    // await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('and some more test text by test user');
-    await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
-    // await page.waitForTimeout(5000)
-    await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
-    await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('and some more test by test user here');
-    await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
-    await page.waitForTimeout(5000)
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('and some more test text by test user');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('and some more text by test user here');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await page.waitForTimeout(5000)
 
-    await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' File' }).click();
-    await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' History', exact: true }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' File' }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' History', exact: true }).click();
 
-    await page.frameLocator('#sbox-iframe').locator('.cp-toolbar-history-previous').nth(1).click();
-    await expect(page.frameLocator('#sbox-iframe').getByText('and some more test text by test user')).toHaveCount(0)
-    await expect(page.frameLocator('#sbox-iframe').getByText('and some more test by test user here')).toHaveCount(0)
+//     await page.frameLocator('#sbox-iframe').locator('.cp-toolbar-history-previous').nth(1).click();
+//     await expect(page.frameLocator('#sbox-iframe').getByText('and some more test text by test user')).toHaveCount(0)
+//     await expect(page.frameLocator('#sbox-iframe').getByText('and some more text by test user here')).toHaveCount(0)
 
-    await expect(page.frameLocator('#sbox-iframe').getByText('some test text by anon')).toBeVisible()
-    await expect(page.frameLocator('#sbox-iframe').getByText('some more test text by anon!')).toBeVisible()
+//     await expect(page.frameLocator('#sbox-iframe').getByText('some test text by anon')).toBeVisible()
+//     await expect(page.frameLocator('#sbox-iframe').getByText('some more test text by anon!')).toBeVisible()
 
-    await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: `pad - file menu - history (previous author)`, status: 'passed',reason: 'Can create Rich Text document and view history (previous author)'}})}`);
-  } catch (e) {
-    console.log(e);
-    await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: `pad - file menu - history (previous author)`, status: 'failed',reason: 'Can\'t create Rich Text document and view history (previous author)'}})}`);
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: `pad - file menu - history (previous author)`, status: 'passed',reason: 'Can create Rich Text document and view history (previous author)'}})}`);
+//   } catch (e) {
+//     console.log(e);
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: `pad - file menu - history (previous author)`, status: 'failed',reason: 'Can\'t create Rich Text document and view history (previous author)'}})}`);
 
-  }  
-});
+//   }  
+// });
+
+// test(`kanban - share at a moment in history`, async ({ }) => {
+
+//   try {
+
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().waitFor()
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('One moment in history');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await expect(page.frameLocator('#sbox-iframe').getByText('One moment in history')).toBeVisible();
+//     await page.waitForTimeout(10000)
+//     await page.frameLocator('#sbox-iframe').getByRole('main').filter({ hasText: 'One moment in history' }).getByAltText('Edit this card').first().click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Delete' }).click();
+//     await page.frameLocator('#sbox-iframe').getByText('Are you sure?').click();
+
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().waitFor()
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('Another moment in history');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await expect(page.frameLocator('#sbox-iframe').getByText('Another moment in history')).toBeVisible();
+//     await page.waitForTimeout(10000)
+//     await page.frameLocator('#sbox-iframe').getByRole('main').filter({ hasText: 'Another moment in history' }).getByAltText('Edit this card').first().click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Delete' }).click();
+//     await page.frameLocator('#sbox-iframe').getByText('Are you sure?').click();
+
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().waitFor()
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('Yet another moment in history');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await expect(page.frameLocator('#sbox-iframe').getByText('Yet another moment in history')).toBeVisible();
+//     await page.waitForTimeout(10000)
+//     await page.frameLocator('#sbox-iframe').getByRole('main').filter({ hasText: 'Yet another moment in history' }).getByAltText('Edit this card').first().click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Delete' }).click();
+//     await page.frameLocator('#sbox-iframe').getByText('Are you sure?').click();
+
+
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' File' }).click();
+    
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' History', exact: true }).click();
+//     await page.frameLocator('#sbox-iframe').locator('.cp-toolbar-history-previous').last().click();
+//     await page.frameLocator('#sbox-iframe').locator('.cp-toolbar-history-previous').last().click();
+
+//     await expect(page.frameLocator('#sbox-iframe').getByText('Another moment in history')).toBeVisible();
+
+
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Share' }).click();
+//     await page.frameLocator('#sbox-secure-iframe').getByText('Link', { exact: true }).click();
+//     await page.frameLocator('#sbox-secure-iframe').locator('#cp-share-link-preview').click();
+//     await page.frameLocator('#sbox-secure-iframe').getByRole('button', { name: ' Copy link' }).click();
+
+//     const clipboardText = await page.evaluate("navigator.clipboard.readText()");
+//     const page1 = await browser.newPage();
+//     await page1.goto(`${clipboardText}`)
+
+//     await expect(page1.frameLocator('#sbox-iframe').getByText('Another moment in history')).toBeVisible();
+
+
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'pad - share at a moment in history', status: 'passed',reason: 'Can share Rich Text at a specific moment in history'}})}`);
+
+//   } catch (e) {
+//     console.log(e);
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'pad - share at a moment in history', status: 'failed',reason: 'Can share Rich Text at a specific moment in history'}})}`);
+
+//   }  
+// });
+
+// test(`pad - save as and import template`, async ({ }) => {
+
+//   try {
+
+//     await page.goto(`${url}/login/`)
+//     await page.getByPlaceholder('Username').fill('test-user');
+//     await page.waitForTimeout(10000)
+//     await page.getByPlaceholder('Password', {exact: true}).fill('newpassword');
+//     const login = page.locator(".login")
+//     await login.waitFor({ timeout: 18000 })
+//     await expect(login).toBeVisible({ timeout: 1800 })
+//     if (await login.isVisible()) {
+//       await login.click()
+//     }
+//     await expect(page).toHaveURL(`${url}/drive/#`, { timeout: 100000 })
+//     await page.waitForTimeout(10000)
+
+//     await page.goto(`${url}/kanban/`)
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Create' }).click();
+
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().waitFor()
+//     await page.frameLocator('#sbox-iframe').locator('.kanban-title-button').first().click();
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').fill('example item');
+//     await page.frameLocator('#sbox-iframe').locator('#kanban-edit').press('Enter');
+//     await expect(page.frameLocator('#sbox-iframe').getByText('example item')).toBeVisible();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' File' }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Save as template', exact: true }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('textbox').fill('example kanban template');
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'OK (enter)' }).click();
+//     await page.waitForTimeout(3000)
+//     await page.goto(`${url}/kanban/`);
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Create' }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' File' }).click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Import a template', exact: true }).click();
+//     await page.frameLocator('#sbox-secure-iframe').getByText('example kanban template').click();
+//     await expect(page.frameLocator('#sbox-iframe').getByText('example item')).toBeVisible();
+
+//     await page.goto(`${url}/drive/`);
+//     await page.frameLocator('#sbox-iframe').getByText('Templates').click();
+//     await page.frameLocator('#sbox-iframe').getByText('example kanban template').click({button: 'right'});
+//     await page.frameLocator('#sbox-iframe').getByText('Destroy').click();
+//     await page.frameLocator('#sbox-iframe').getByRole('button', { name: 'OK (enter)' }).click();
+//     await expect(page.frameLocator('#sbox-secure-iframe').getByText('example kanban template')).toHaveCount(0)
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'pad > save as template', status: 'passed',reason: 'Can save and use Rich Text document as template'}})}`);
+
+//   } catch (e) {
+//     console.log(e);
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'pad > save as template', status: 'failed',reason: 'Can\'t save and use Rich Text document as template'}})}`);
+
+//   }  
+// });
+
+// test(`kanban - can drag boards`, async ({ }) => {
+
+//   try {
+
+//     await page.frameLocator('#sbox-iframe').getByRole('banner').filter({ hasText: 'To Do' }).dragTo(page.frameLocator('#sbox-iframe').getByRole('banner').filter({ hasText: 'Done' }))
+//     await expect(page).toHaveScreenshot({ maxDiffPixels: 1800 });
+
+    
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'kanban - can drag boards', status: 'passed',reason: 'Can drag Kanban boards'}})}`);
+
+//   } catch (e) {
+//     console.log(e);
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'kanban - can drag boards', status: 'failed',reason: 'Can\'t drag Kanban boards'}})}`);
+
+//   }  
+// });
+
+// test(`kanban - can drag items`, async ({ }) => {
+
+//   try {
+
+//     await page.frameLocator('#sbox-iframe').locator('div').filter({ hasText: /^Item 1$/ }).first().dragTo(page.frameLocator('#sbox-iframe').locator('div').filter({ hasText: /^Item 2$/ }).first())
+//     await expect(page).toHaveScreenshot({ maxDiffPixels: 1800 });
+
+    
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'kanban - can drag boards', status: 'passed',reason: 'Can drag Kanban items'}})}`);
+
+//   } catch (e) {
+//     console.log(e);
+//     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'kanban - can drag boards', status: 'failed',reason: 'Can\'t drag Kanban items'}})}`);
+
+//   }  
+// });
 
 test.afterEach(async ({  }) => {
   await browser.close()
--- e2e-tests/markdown_anon.spec.js
+++ e2e-tests/markdown_anon.spec.js
@@ -1,15 +1,29 @@
 const { test, expect } = require('@playwright/test');
 const { firefox, chromium, webkit } = require('@playwright/test');
 const { url, titleDate } = require('../browserstack.config.js')
+var fs = require('fs');
 
-let browser;
+
 let page;
+let pageOne;
+let browser;
 
-test.beforeEach(async ({ page }, testInfo) => {
+test.beforeEach(async ({  }, testInfo) => {
+  
   test.setTimeout(2400000);
-  await page.goto(`${url}/slide`);
-});
+  const name = testInfo.project.name
+  if (name.indexOf('firefox') !== -1 ) {
+    browser = await firefox.launch();
+  } else if (name.indexOf('webkit') !== -1 ) {
+    browser = await webkit.launch();
+  } else {
+    browser = await chromium.launch();
+  }
 
+  page = await browser.newPage();
+  await page.goto(`${url}/slide`)
+  await page.waitForTimeout(5000)
+});
 
 test('markdown - anon - input text into editor', async ({ page }) => {
   
@@ -26,4 +40,23 @@ test('markdown - anon - input text into editor', async ({ page }) => {
     await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'markdown', status: 'failed',reason: 'Can\'t anonymously create Markdown slides'}})}`);
 
   }  
+});
+
+test('markdown - input text into editor', async ({ page }) => {
+  
+  try {
+
+    await page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Tools' }).click();
+    await expect(page.frameLocator('#sbox-iframe').locator('.cp-markdown-toolbar')).toBeVisible();
+   
+    await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'markdown - input text into editor', status: 'passed',reason: 'Can input '}})}`);
+  } catch (e) {
+    console.log(e);
+    await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {name: 'markdown - input text into editor', status: 'failed',reason: 'Can\'t anonymously create Markdown slides'}})}`);
+
+  }  
+});
+
+test.afterEach(async ({  }) => {
+  await browser.close()
 });
