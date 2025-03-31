c516e485935ad197982ea493737f0ae66dcfb403
zuzanna-maria <zuzanna.ludzik@gmail.com>
2025-03-31 10:44:12 +0200

    WIP

--- e2e-tests/fileactions.js
+++ e2e-tests/fileactions.js
@@ -7,76 +7,94 @@ export class FileActions {
   /**
 	* @param {import('@playwright/test').Page} page
 	*/
-  constructor (page) {
+  constructor (page, testName, mobile) {
     this.page = page;
 
-    // user actions
+    // user actions-related locators
     this.login = page.locator('.login');
     this.register = page.locator("[id='register']");
-    this.loginLink = page.frameLocator('#sbox-iframe').getByRole('link', { name: 'Log in' });
-    this.registerLink = page.frameLocator('#sbox-iframe').getByRole('link', { name: 'Sign up' });
+    this.loginLink = this.mainFrame.getByRole('link', { name: 'Log in' });
+    this.registerLink = this.mainFrame.getByRole('link', { name: 'Sign up' });
 
-    // drive
-    this.drivemenu = page.frameLocator('#sbox-iframe').locator('.cp-toolbar-user-dropdown.cp-dropdown-container');
-    this.driveadd = page.frameLocator('#sbox-iframe').locator('#cp-app-drive-content-folder span').first();
-    this.notifications = page.frameLocator('#sbox-iframe').getByLabel('Notifications');
-    this.driveContentFolder = page.frameLocator('#sbox-iframe').locator('#cp-app-drive-content-folder');
-    this.newFile = page.frameLocator('#sbox-iframe').getByRole('listitem').filter({ hasText: /^New$/ });
-    this.settings = page.frameLocator('#sbox-iframe').getByText('Settings');
-    this.driveSideMenu = page.frameLocator('#sbox-iframe').locator('#cp-app-drive-tree');
+    // drive locators
+    this.drivemenu = this.mainFrame.locator('.cp-toolbar-user-dropdown.cp-dropdown-container');
+    this.driveadd = this.mainFrame.locator('#cp-app-drive-content-folder span').first();
+    this.notifications = this.mainFrame.getByLabel('Notifications');
+    this.driveContentFolder = this.mainFrame.locator('#cp-app-drive-content-folder');
+    this.newFile = this.mainFrame.getByRole('listitem').filter({ hasText: /^New$/ });
+    this.settings = this.mainFrame.getByText('Settings');
+    this.driveSideMenu = this.mainFrame.locator('#cp-app-drive-tree');
 
-    // file actions
-    this.filemenu = page.frameLocator('#sbox-iframe').getByRole('button', { name: ' File' });
-    this.filemenuMobile = page.frameLocator('#sbox-iframe').locator('.cp-toolbar-file');
-    this.fileimport = page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Import' }).locator('a');
-    this.filecopy = page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Make a copy' }).locator('a');
-    this.historyPrev = page.frameLocator('#sbox-iframe').locator('.cp-toolbar-history-previous').last();
-    this.toolbar = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Tools' });
-    this.shareLink = page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Share' });
+    // file locators
+
+    this.page = page;
+    this.testName = testName;
+    this.mobile = mobile;
+    
+
+    // locators
+    this.mainFrame = page.frameLocator('#sbox-iframe');
+    this.newFile = this.mainFrame.getByRole('menuitem', { name: 'New' }).locator('a');
+    this.storeFile = this.mainFrame.getByRole('menuitem', { name: 'Store' }).locator('a');
+    this.trashFile = this.mainFrame.getByRole('menuitem', { name: 'Move to trash' }).locator('a');
+    this.alertMessage = this.mainFrame.locator('.alertify').locator('.msg');
+    this.okButton = this.mainFrame
+      .locator('.dialog')
+      .getByRole('button', { name: 'OK (enter)' })
+      .first();
+    this.cancelButton = this.mainFrame.getByRole('button', { name: 'Cancel (esc)' });
+    this.storageSuccess = this.mainFrame.locator('alertify-logs');
+
+    this.shareButton = this.mainFrame.getByRole('button', { name: 'Share' });
+
+    this.fileName = this.mainFrame.locator('.cp-toolbar-title');
+    this.titleEditBox = this.mainFrame.locator('.cp-toolbar-title-edit > .fa');
+    this.titleInput = this.mainFrame.locator('.cp-toolbar-title').locator('input');
+    this.saveTitle = this.mainFrame.locator('.cp-toolbar-title-save');
+
+    this.filemenu = this.mainFrame.getByRole('button', { name: ' File' });
+    this.filemenuMobile = this.mainFrame.locator('.cp-toolbar-file');
+    this.fileimport = this.mainFrame.getByRole('menuitem', { name: ' Import' }).locator('a');
+    this.filecopy = this.mainFrame.getByRole('menuitem', { name: ' Make a copy' }).locator('a');
+    this.historyPrev = this.mainFrame.locator('.cp-toolbar-history-previous').last();
+    this.toolbar = this.mainFrame.getByRole('button', { name: 'Tools' });
+    this.shareLink = this.mainFrame.getByRole('button', { name: ' Share' });
     this.shareSecureLink = page.frameLocator('#sbox-secure-iframe').getByRole('button', { name: ' Share' });
     this.shareCopyLink = page.frameLocator('#sbox-secure-iframe').getByRole('button', { name: ' Copy link' });
-    this.filesaved = page.frameLocator('#sbox-iframe').getByText('Saved').nth(0);
-    this.deletebutton = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Delete' });
-    // this.trash = page.frameLocator('#sbox-iframe').getByRole('listitem').filter({ hasText: 'Move to trash' });
-    this.trash = page.frameLocator('#sbox-iframe').getByText('Move to trash')
+    this.filesaved = this.mainFrame.getByText('Saved');
+    this.deletebutton = this.mainFrame.getByRole('button', { name: 'Delete' });
+    this.trash = this.mainFrame.getByText('Move to trash')
 
-    this.destroy = page.frameLocator('#sbox-iframe').getByRole('listitem').filter({ hasText: 'Destroy' });
+    this.destroy = this.mainFrame.getByRole('listitem').filter({ hasText: 'Destroy' });
     this.linkTab = page.frameLocator('#sbox-secure-iframe').locator('#cp-tab-link');
     this.linkTabMobile = page.frameLocator('#sbox-secure-iframe').getByLabel('Link');
-    this.createFile = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Create' });
-    this.oneTimeLocal = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'One time ' })
-    this.oneTime = page.frameLocator('#sbox-iframe').getByRole('button', { name: ' One time' })
+    this.createFile = this.mainFrame.getByRole('button', { name: 'Create' });
+    this.oneTimeLocal = this.mainFrame.getByRole('button', { name: 'One time ' })
+    this.oneTime = this.mainFrame.getByRole('button', { name: ' One time' })
 
     // buttons
     this.closeButtonSecure = page.frameLocator('#sbox-secure-iframe').getByRole('button', { name: 'Close' });
-    this.closeButton = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Close' });
+    this.closeButton = this.mainFrame.getByRole('button', { name: 'Close' });
     this.okButtonSecure = page.frameLocator('#sbox-secure-iframe').getByRole('button', { name: 'OK (enter)' });
-    this.okButton = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'OK (enter)' });
+    this.okButton = this.mainFrame.getByRole('button', { name: 'OK (enter)' });
 
-    // this.preview =
 
     // code
-    this.codeeditor = page.frameLocator('#sbox-iframe').locator('.CodeMirror-scroll');
-    this.codepreview = page.frameLocator('#sbox-iframe').locator('#cp-app-code-preview-content');
+    this.codeeditor = this.mainFrame.locator('.CodeMirror-scroll');
+    this.codepreview = this.mainFrame.locator('#cp-app-code-preview-content');
 
     // form
-    this.copyPublicLink = page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Copy public link' });
+    this.copyPublicLink = this.mainFrame.getByRole('button', { name: 'Copy public link' });
 
     // pad
-    this.padeditor = page.frameLocator('#sbox-iframe').frameLocator('iframe[title="Editor\\, editor1"]');
+    this.padeditor = this.mainFrame.frameLocator('iframe[title="Editor\\, editor1"]');
 
     // markdown
-    this.slideeditor = page.frameLocator('#sbox-iframe').locator('.CodeMirror-code');
+    this.slideeditor = this.mainFrame.locator('.CodeMirror-code');
   }
 
   async moveToTrash() {
-    // if (local) {
-    //   await this.trashLocal.click()
-    
-    // } else {
       await this.trash.click()
-    // }
-  
   }
 
   async oneTimeClick(local) {
@@ -89,19 +107,11 @@ export class FileActions {
   }
 
   async title (title) {
-    return this.page.frameLocator('#sbox-iframe').locator('.cp-toolbar-title').getByText(`${title}`);
+    return this.this.mainFrame.locator('.cp-toolbar-title').getByText(`${title}`);
   }
 
-  // async titleDate (mobile, isBrowserstack) {
-  //   if (mobile && isBrowserstack || !mobile && !isBrowserstack) {
-  //     return titleDateComma;
-  //   } else {
-  //     return titleDate;
-  //   }
-  // }
-
   async clickTags (local) {
-    await this.page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Tags' }).locator('a').click();
+    await this.this.mainFrame.getByRole('menuitem', { name: ' Tags' }).locator('a').click();
   }
 
   async clickLinkTab (mobile) {
@@ -114,11 +124,11 @@ export class FileActions {
 
   async responses (visible) {
     if (visible) {
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Responses (1)' }).click();
+      await this.this.mainFrame.getByRole('button', { name: ' Responses (1)' }).click();
     } else {
       await this.page.reload();
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Responses (1)' }).waitFor()
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Responses (1)' }).click();
+      await this.this.mainFrame.getByRole('button', { name: ' Responses (1)' }).waitFor()
+      await this.this.mainFrame.getByRole('button', { name: ' Responses (1)' }).click();
     }
   }
 
@@ -136,19 +146,19 @@ export class FileActions {
 
   async togglePreview (mobile) {
     if (mobile) {
-      await this.page.frameLocator('#sbox-iframe').locator('.cp-toolbar-rightside-button').locator('.fa.fa-eye').click();
+      await this.this.mainFrame.locator('.cp-toolbar-rightside-button').locator('.fa.fa-eye').click();
     } else {
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Preview' }).click();
+      await this.this.mainFrame.getByRole('button', { name: 'Preview' }).click();
     }
   }
 
   async toggleTools (mobile) {
     if (mobile) {
-      await this.page.frameLocator('#sbox-iframe').locator('.cp-toolbar-tools').waitFor();
-      await this.page.frameLocator('#sbox-iframe').locator('.cp-toolbar-tools').click();
+      await this.this.mainFrame.locator('.cp-toolbar-tools').waitFor();
+      await this.this.mainFrame.locator('.cp-toolbar-tools').click();
     } else {
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Tools' }).waitFor();
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: 'Tools' }).click();
+      await this.this.mainFrame.getByRole('button', { name: 'Tools' }).waitFor();
+      await this.this.mainFrame.getByRole('button', { name: 'Tools' }).click();
     }
   }
 
@@ -165,8 +175,8 @@ export class FileActions {
 
   async share (mobile) {
     if (mobile) {
-      await this.page.frameLocator('#sbox-iframe').locator('.cp-toolar-share-button').waitFor()
-      await this.page.frameLocator('#sbox-iframe').locator('.cp-toolar-share-button').click();
+      await this.this.mainFrame.locator('.cp-toolar-share-button').waitFor()
+      await this.this.mainFrame.locator('.cp-toolar-share-button').click();
     } else {
       await this.shareLink.waitFor()
       await this.shareLink.click();
@@ -175,49 +185,132 @@ export class FileActions {
 
   async access (mobile) {
     if (mobile) {
-      await this.page.frameLocator('#sbox-iframe').locator('.cp-toolar-access-button').click();
+      await this.this.mainFrame.locator('.cp-toolar-access-button').click();
     } else {
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Access' }).click();
+      await this.this.mainFrame.getByRole('button', { name: ' Access' }).click();
     }
   }
 
   async history (mobile) {
     await this.filemenuClick(mobile);
-    await this.page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' History' }).locator('a').click();
+    await this.this.mainFrame.getByRole('menuitem', { name: ' History' }).locator('a').click();
   }
 
   async export (mobile) {
     await this.filemenuClick(mobile);
-    await this.page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Export' }).locator('a').click();
+    await this.this.mainFrame.getByRole('menuitem', { name: ' Export' }).locator('a').click();
   }
 
   async importTemplate (mobile, local) {
     await this.filemenuClick(mobile);
     console.log()
     if (local) {
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Import a template', exact: true }).waitFor()
-      await this.page.frameLocator('#sbox-iframe').getByRole('button', { name: ' Import a template', exact: true }).click();
+      await this.this.mainFrame.getByRole('button', { name: ' Import a template', exact: true }).waitFor()
+      await this.this.mainFrame.getByRole('button', { name: ' Import a template', exact: true }).click();
     } else {
-      await this.page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Import a template' }).locator('a').waitFor()
-      await this.page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Import a template' }).locator('a').click();
+      await this.this.mainFrame.getByRole('menuitem', { name: ' Import a template' }).locator('a').waitFor()
+      await this.this.mainFrame.getByRole('menuitem', { name: ' Import a template' }).locator('a').click();
     }
   }
 
   async saveTemplate (mobile, local) {
     await this.filemenuClick(mobile);
-    await this.page.frameLocator('#sbox-iframe').getByRole('menuitem', { name: ' Save as template' }).locator('a').click();
+    await this.this.mainFrame.getByRole('menuitem', { name: ' Save as template' }).locator('a').click();
   }
 
-  async accessTeam (mobile, local) {
-    await this.page.frameLocator('#sbox-iframe').locator('#cp-sidebarlayout-rightside').getByText('test team').waitFor();
-    await this.page.waitForTimeout(2000);
-    await this.page.frameLocator('#sbox-iframe').locator('#cp-sidebarlayout-rightside').getByText('test team').click();
-    console.log(await this.page.frameLocator('#sbox-iframe').locator('div').filter({ hasText: /^Members$/ }).locator('span').first().isVisible())
-    if (!await this.page.frameLocator('#sbox-iframe').locator('div').filter({ hasText: /^Members$/ }).locator('span').first().isVisible()){
-      while (!await this.page.frameLocator('#sbox-iframe').locator('div').filter({ hasText: /^Members$/ }).locator('span').first().isVisible()) {
-        await this.page.waitForTimeout(2000);
-        await this.page.frameLocator('#sbox-iframe').locator('#cp-sidebarlayout-rightside').getByText('test team').click();
-      }
+  
+}
+
+class NewFileModal {
+  constructor (filePage) {
+    this.filePage = filePage;
+    this.modalRoot = filePage.mainFrame.locator('#cp-app-toolbar-creation-dialog');
+    this.close = this.modalRoot.locator('.cp-modal-close');
+  }
+
+  async createFileOfType (context, fileType) {
+    // For new tabs, see https://playwright.dev/docs/pages#handling-new-pages
+    const pagePromise = context.waitForEvent('page');
+    await this.iconLocator(fileType).click();
+    const nextPage = await pagePromise;
+    return new FilePage(nextPage, this.filePage.testName, this.filePage.mobile);
+  }
+
+  iconLocator (fileType) {
+    return this
+      .filePage
+      .mainFrame
+      .getByText(
+        this.iconName(fileType),
+        { exact: true }
+      );
+  }
+
+  iconName (fileType) {
+    switch (fileType) {
+      case 'pad':
+        return 'Rich text';
+      case 'slide':
+        return 'Markdown slides';
+      default:
+        return `${fileType.charAt(0).toUpperCase() + fileType.slice(1)}`;
     }
   }
 }
+
+
+class ShareFileModal {
+  constructor (filePage) {
+    this.filePage = filePage;
+    this.shareFrame = filePage.page.frameLocator('#sbox-secure-iframe');
+    this.openButton = this.shareFrame.getByRole('button', { name: 'Open Link' });
+    this.copyButton = this.shareFrame.getByRole('button', { name: 'Copy Link' });
+  }
+
+  toggle (text) {
+    return this.shareFrame.getByText(text, { exact: true });
+  }
+
+  viewToggle (fileType) {
+    if (fileType === 'form') {
+      return this.toggle('Participant');
+    } else {
+      return this.toggle('View');
+    }
+  }
+
+  async getLinkAfterCopy () {
+    return await this.filePage.page.evaluate('navigator.clipboard.readText()');
+  }
+
+  async openLinkClick (context) {
+    const pagePromise = context.waitForEvent('page');
+    await this.openButton.click();
+    const nextPage = await pagePromise;
+    return new FilePage(nextPage, this.filePage.testName, this.filePage.mobile);
+  }
+}
+
+class ChatModal {
+  constructor (filePage) {
+    this.filePage = filePage;
+    this.chatPane = this.filePage.mainFrame.locator('.cp-app-contacts-chat');
+    this.chatInput = this.chatPane.getByPlaceholder('Type a message here...');
+  }
+
+  async enterText (text) {
+    await this.chatInput.click();
+    await this.chatInput.fill(text);
+    await this.chatInput.press('Enter');
+  }
+}
+
+class StoreModal {
+  constructor (filePage) {
+    this.filePage = filePage;
+    this.storePane = this.filePage.mainFrame.locator('.cp-corner-container');
+    this.dismissButton = this.storePane.getByRole('button', { name: 'Don\'t store', exact: true });
+    this.storeButton = this.storePane.getByRole('button', { name: 'Store', exact: true });
+  }
+}
+
