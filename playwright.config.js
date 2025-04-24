9aa8f476df1f1b7176a78da4e609a866cb8d9c9a
zuzanna-maria <zuzanna.ludzik@gmail.com>
2025-04-24 20:46:40 +0200

    test

--- playwright.config.js
+++ playwright.config.js
@@ -46,7 +46,8 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], 
   // ['@estruyf/github-actions-reporter']
-  ['playwright-ctrf-json-reporter', {outputFile: 'ctrf.json'}]
+  ['playwright-ctrf-json-reporter', {outputFile: 'ctrf.json'},
+  ['github-actions-ctrf',{outputFile: 'ctrf.json'} ]]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
