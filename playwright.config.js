bc3307d82e288fd193e9e3158fb58676abbd3776
zuzanna-maria <zuzanna.ludzik@gmail.com>
2025-04-24 16:39:26 +0200

    test

--- playwright.config.js
+++ playwright.config.js
@@ -44,7 +44,10 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['playwright-ctrf-json-reporter', {}]],
+  reporter: [['html'], 
+  ['@estruyf/github-actions-reporter']
+  // ['playwright-ctrf-json-reporter', {}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
