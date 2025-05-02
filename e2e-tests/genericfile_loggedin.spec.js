12d4e08fd35866ea50cf0abdb01eb0c0726e7e24
zuzanna-maria <zuzanna.ludzik@gmail.com>
2025-05-02 15:57:18 +0200

    remote VAD

--- e2e-tests/genericfile_loggedin.spec.js
+++ e2e-tests/genericfile_loggedin.spec.js
@@ -42,8 +42,8 @@ test.beforeEach(async ({ page, isMobile }, testInfo) => {
 
 });
 
-const docNames = ['pad', 'sheet', 'code', 'slide', 'kanban', 'whiteboard', 'form', 'diagram'];
-// const docNames = ['pad'];
+// const docNames = ['pad', 'sheet', 'code', 'slide', 'kanban', 'whiteboard', 'form', 'diagram'];
+const docNames = ['pad'];
 
 
 docNames.forEach(function (name) {
